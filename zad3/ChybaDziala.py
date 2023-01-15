4c1af797097f32681df632f8bf640ca09d078665
Jakub Świerczyński 242552 <242552@edu.p.lodz.pl>
2023-01-15 23:28:03 +0100

    poprawione do 4 wymiarow + wykresy + wcss

--- zad3/ChybaDziala.py
+++ zad3/ChybaDziala.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 from matplotlib import pyplot as plt
 
+
 liczba_klastrow = 3
 dane = pd.read_csv("data_test.csv", header=None)
     # centroidy = []
@@ -54,18 +55,16 @@ def k_srednich(lista1, lista2, lista3, lista4):
         pom.append(lista4[i])
         lista.append(pom)
 
-
     #1. początkowe położenia centroidów
     losoweCentroidy(lista, centroidy)
 
     #2. przypisanie punktowi najbliższego centroida
     klastry=przypisanieDoKlastrow(klastry, lista, centroidy)
-    print("klastry1",klastry[0],'\n')
-    print("klastry2",klastry[1],'\n')
-    print("klastry3",klastry[2],'\n')
+    # print("klastry1",klastry[0],'\n')
+    # print("klastry2",klastry[1],'\n')
+    # print("klastry3",klastry[2],'\n')
 
     #3. aktualizacja położenia centroidów
-    # for k in range(liczba_klastrow):
     noweCentroidy = []
     pierwszeWejscie=True
     while(centroidy!=noweCentroidy):
@@ -84,12 +83,12 @@ def k_srednich(lista1, lista2, lista3, lista4):
                 avgY+=klastry[i][j][1]
                 avgZ+=klastry[i][j][2]
                 avgW+=klastry[i][j][3]
-            # if not klastry[i]:
-            #     pom1=-1
-            #     pom2=-1
-            #     pom3=-1
-            #     pom4=-1
-            #     return pom1,pom2,pom3,pom4
+            if not klastry[i]:
+                pom1=-1
+                pom2=-1
+                # pom3=-1
+                # pom4=-1
+                return pom1,pom2
             # print("klastry[i]",klastry[i])
             # print(len(klastry[i]))
             avgX/=len(klastry[i])
@@ -117,18 +116,28 @@ def doWykresu(lista):
         y.append(i[1])
         z.append(i[2])
         w.append(i[3])
-    # print("x", x)
-    # print("y", y)
     return x,y,z,w
 
-# def test(lista1, lista2, lista3, lista4):
-#     pom1 = -1
-#     pom2 = -1
-#     while(pom1==-1 or pom2==-1):
-#         pom1, pom2 = k_srednich(lista1, lista2, lista3, lista4)
-#     return pom1,pom2
+def test(lista1, lista2, lista3, lista4):
+    pom1 = -1
+    pom2 = -1
+    while(pom1==-1 or pom2==-1):
+        pom1, pom2 = k_srednich(lista1, lista2, lista3, lista4)
+    return pom1,pom2
 
-centroidy, klastry = k_srednich(dane[0], dane[1], dane[2],dane[3])
+def wcss(klastry, centroidy):
+    suma=0
+    print(len(klastry))
+    for i in range(len(klastry)):
+        for j in range(len(klastry[i])):
+            for k in range(4):
+                suma+=(klastry[i][j][k]-centroidy[i][k])**2
+    return round(suma, 3)
+
+
+
+
+centroidy, klastry = test(dane[0], dane[1], dane[2],dane[3])
 print("centr", centroidy)
 tmp = []
 for i in range(3):
@@ -142,41 +151,28 @@ for i in range(3):
 print("tmp",tmp)
 for i in range(4):
     for j in range(i+1,4):
-
-        # centroidy, klastry = test(dane[i], dane[j])
         print("i",i," j",j)
 
         plt.figure(figsize=(5,4),dpi=300)
-        # #grupa + kolor
+        #grupa1
         plt.scatter(tmp[0][i],tmp[0][j],s=80, color='red',label="grupa 1")
         plt.scatter(centroidy[0][i],centroidy[0][j],s=80, color='red',label="grupa 1", marker="D", edgecolor="black")
-        #
-        #
-        # #grupa + kolor
+
+        # #grupa2
         plt.scatter(tmp[1][i],tmp[1][j],s=80, color='green',label="grupa 2")
         plt.scatter(centroidy[1][i],centroidy[1][j],s=80, color='green',label="grupa 2", marker="D", edgecolor="black")
 
-        #grupa + kolor
+        #grupa3
         plt.scatter(tmp[2][i],tmp[2][j],s=80, color='yellow',label="grupa 3")
         plt.scatter(centroidy[2][i],centroidy[2][j],s=80, color='yellow',label="grupa 3", marker="D", edgecolor="black")
         plt.show()
 
-
-# centroidy, klastry = k_srednich(dane[0], dane[1], dane[2],dane[3])
-# print("cent",centroidy)
-# # centroidy, klastry = test(dane[i], dane[j])
-# # print("i",i," j",j)
-# x,y = doWykresu(klastry[0])
-# plt.figure(figsize=(5,4),dpi=300)
-# #grupa 1
-# plt.scatter(x,y,s=80, color='red',label="grupa 1")
-# plt.scatter(centroidy[0][0],centroidy[0][1],s=80, color='red',label="grupa 1", marker="D", edgecolor="black")
-# #grupa2
-# x,y = doWykresu(klastry[1])
-# plt.scatter(x,y,s=80, color='green',label="grupa 2")
-# plt.scatter(centroidy[1][0],centroidy[1][1],s=80, color='green',label="grupa 2", marker="D", edgecolor="black")
-# #grupa3
-# x,y = doWykresu(klastry[2])
-# plt.scatter(x,y,s=80, color='yellow',label="grupa 3")
-# plt.scatter(centroidy[2][0],centroidy[2][1],s=80, color='yellow',label="grupa 3", marker="D", edgecolor="black")
-# plt.show()
+wartoscWCSS = [0,0,0,0,0,0,0,0,0]
+for i in range(2, 11):
+    liczba_klastrow=i
+    centroidy, klastry=test(dane[0], dane[1], dane[2],dane[3])
+    wartoscWCSS[i-2] = wcss(klastry, centroidy)
+print(wartoscWCSS)
+plt.figure(figsize=(5,4),dpi=300)
+plt.plot([2,3,4,5,6,7,8,9,10],wartoscWCSS,linestyle='-')
+plt.show()
